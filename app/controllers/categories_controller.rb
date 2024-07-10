5db220b53f3efb0bae88cf061e0a9b5da0dc47e8
micahyap <yapmicahsy@gmail.com>
2024-07-10 13:25:00 +0800

    journal controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,47 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only [:show, :edit, :update]
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to category_url(@category), notice: "Created successfully!"}
+
+      else
+        format.html {render :new, status: :unprocessable_entity, notice: "Creation failed!"}
+      end
+    end  
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to category_url(@category), notice: "Updated successfully" }
+      else 
+        format.html { respond :edit, status: :unprocessable_entity, notice: "Update failed!"}
+      end
+    end
+  end
+
+
+end
+
+private
+def set_category
+  @category =  Category.find(params[:id])
+end
+
+def category_params
+  params.require(:category).permit(:task, :description)
+end
