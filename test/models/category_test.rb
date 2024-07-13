8e44f7079ebea5a86a6b0d34974fff924c2bf6ae
micahyap <yapmicahsy@gmail.com>
2024-07-13 10:11:54 +0800

    added some model test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -3,11 +3,16 @@ require "test_helper"
 class CategoryTest < ActiveSupport::TestCase
   test 'should not save without task name' do
     category = Category.new(description: 'description here')
-    assert_not category.save, 'Saved without task name!'
+    assert_not category.save, 'Saved the category without a task name!'
   end
 
   test 'should not save without description' do
     category = Category.new(task: 'task name here')
-    assert_not category.save, 'Saved without description!'
+    assert_not category.save, 'Saved the category without a description!'
+  end
+
+  test 'should save with both task name and description' do
+    category = Category.new(task: 'task name here', description: 'description here')
+    assert category.save, 'Could not save the category with both task name and description'
   end
 end
