afb8238aa0542c71551cb9fd09da41bb19d26dbf
micahyap <yapmicahsy@gmail.com>
2024-07-18 19:40:06 +0800

    controller and model tests

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,7 +1,51 @@
 require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Rails.application.routes.url_helpers
+
+  setup do
+    @category = categories(:one)  
+    @task = tasks(:one) 
+  end
+
+  test "should get index" do
+    get category_tasks_url(@category)
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_category_task_path(@category)
+    assert_response :success
+  end
+
+  test "should create task" do
+    assert_difference('Task.count') do
+      post category_tasks_path(@category), params: { task: { task: @task.task, description: @task.description, date: @task.date } }
+    end
+
+    assert_redirected_to category_task_path(@category, Task.last)
+  end
+
+  test "should show task" do
+    get category_task_path(@category, @task)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_category_task_path(@category, @task)
+    assert_response :success
+  end
+
+  test "should update task" do
+    patch category_task_path(@category, @task), params: { task: { task: @task.task, description: @task.description, date: @task.date } }
+    assert_redirected_to category_task_path(@category, @task)
+  end
+
+  test "should destroy task" do
+    assert_difference('Task.count', -1) do
+      delete category_task_path(@category, @task)
+    end
+
+    assert_redirected_to category_tasks_path(@category)
+  end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,18 @@
-require "test_helper"
+require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save without task name' do
+    task = Task.new(description: 'description here', date: 'date here')
+    assert_not task.save, 'Saved the task without a task name!'
+  end
+
+  test 'should not save without description' do
+    task = Task.new(task: 'task name here', date: 'date here')
+    assert_not task.save, 'Saved the task without a description!'
+  end
+
+  test 'should save with both task name and description' do
+    task = Task.new(task: 'task name here', description: 'description here', date: 'date here')
+    assert task.save, 'Could not save the task with both task name and description'
+  end
 end
